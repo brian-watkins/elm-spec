e148b3a82fbe28193f835d9fca9ee5aa1c9a85ed
Brian Watkins <brian.watkins@gmail.com>
2019-12-07 18:58:33 -0500

    Added docs

--- src/Spec/Http/Route.elm
+++ src/Spec/Http/Route.elm
@@ -5,23 +5,43 @@ module Spec.Http.Route exposing
   , route
   )
 
+{-| Functions for defining HTTP routes.
 
+@docs HttpRoute
+
+# Define a Route
+@docs get, post, route
+
+-}
+
+
+{-| An HTTP route is an HTTP method plus a URL.
+-}
 type alias HttpRoute =
   { method: String
   , url: String
   }
 
 
+{-| Define a GET route.
+-}
 get : String -> HttpRoute
 get =
   route "GET"
 
 
+{-| Define a POST route.
+-}
 post : String -> HttpRoute
 post =
   route "POST"
 
 
+{-| Define a route with the given method and url.
+
+    Spec.Http.Route.route "PATCH" "http://fake-server.com/fake"
+
+-}
 route : String -> String -> HttpRoute
 route method url =
   { method = method
