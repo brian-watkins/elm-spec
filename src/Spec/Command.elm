6c26a23982dc6999f2aa72f52a8fff476bcc7728
Brian Watkins <brian.watkins@gmail.com>
2019-12-07 18:29:43 -0500

    Added docs

--- src/Spec/Command.elm
+++ src/Spec/Command.elm
@@ -3,16 +3,26 @@ module Spec.Command exposing
   , fake
   )
 
+{-| Functions to work with `Cmd` values in a spec.
+
+@docs send, fake
+-}
+
 import Spec.Step as Step
 import Task
 import Json.Encode as Encode
 
 
+{-| A step that sends a command.
+-}
 send : Cmd msg -> Step.Context model -> Step.Command msg
 send cmd _ =
   Step.sendCommand cmd
 
 
+{-| Generate a `Cmd` value that will send a `Msg` to the `update`
+function provided in the setup of the scenario.
+-}
 fake : msg -> Cmd msg
 fake msg =
   Task.succeed never
