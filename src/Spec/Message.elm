00fcedf55e9073d2a414c1fa70e9c3d52022b0f4
Brian Watkins <brian.watkins@gmail.com>
2019-12-08 09:25:46 -0500

    Added docs

--- src/Spec/Message.elm
+++ src/Spec/Message.elm
@@ -7,10 +7,24 @@ module Spec.Message exposing
   , is
   )
 
+{-| Functions for working with messages that are sent between elm-spec and the JavaScript elm-spec runner.
+
+# Build a Message
+@docs Message, for, withBody
+
+# Read a Message
+@docs belongsTo, is, decode
+
+-}
+
 import Json.Encode as Encode exposing (Value)
 import Json.Decode as Json
 
 
+{-| Represents a message to pass between elm-spec and the JavaScript elm-spec runner.
+
+The body property is a JSON value.
+-}
 type alias Message =
   { home: String
   , name: String
@@ -18,6 +32,18 @@ type alias Message =
   }
 
 
+{-| Build a `Message` with the given home and name.
+
+    for "some-plugin" "some-task"
+
+results in:
+
+    { home = "some-plugin"
+    , name = "some-task"
+    , body = null
+    }
+
+-}
 for : String -> String -> Message
 for home name =
   { home = home
@@ -26,21 +52,37 @@ for home name =
   }
 
 
+{-| Set the `body` of a `Message` to some JSON value
+-}
 withBody : Value -> Message -> Message
 withBody value message =
   { message | body = value }
 
 
+{-| Check whether the message's `home` property is equal to the given string.
+-}
 belongsTo : String -> Message -> Bool
 belongsTo home message =
   message.home == home
 
 
+{-| Check whether the message's `home` and `name` properties match the given strings.
+
+    { home = "some-plugin"
+    , name = "some-task"
+    , body = null
+    }
+      |> is "some-plugin" "some-task"
+
+would return `True`.
+-}
 is : String -> String -> Message -> Bool
 is home name message =
   message.home == home && message.name == name
 
 
+{-| Decode the JSON `body` of the message to a `Maybe` type.
+-}
 decode : Json.Decoder a -> Message -> Maybe a
 decode messageDecoder message =
   Json.decodeValue messageDecoder message.body
