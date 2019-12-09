f4fc38a91e8f5a23629b13bcc61c70e6de1c1c45
Brian Watkins <brian.watkins@gmail.com>
2019-12-08 21:28:50 -0500

    Added docs

--- src/Spec/Time.elm
+++ src/Spec/Time.elm
@@ -5,6 +5,16 @@ module Spec.Time exposing
   , nextAnimationFrame
   )
 
+{-| Functions for working with time during a spec.
+
+# Set Up at a Time
+@docs withTime, withTimezoneOffset
+
+# Pass the Time
+@docs tick, nextAnimationFrame
+
+-}
+
 import Spec.Setup as Setup exposing (Setup)
 import Spec.Setup.Internal as Setup
 import Spec.Step as Step
@@ -14,6 +24,10 @@ import Spec.Markup.Message as Message
 import Json.Encode as Encode
 
 
+{-| Set up the scenario at a particular time.
+
+Provide the number of milliseconds since the UNIX epoch.
+-}
 withTime : Int -> Setup model msg -> Setup model msg
 withTime posix =
   Message.for "_time" "set-time"
@@ -21,6 +35,10 @@ withTime posix =
     |> Setup.configure
 
 
+{-| Set up the scenario at a particular timezone offset.
+
+Provide the timezone offste in minutes.
+-}
 withTimezoneOffset : Int -> Setup model msg -> Setup model msg
 withTimezoneOffset zoneOffset =
   Message.for "_time" "set-timezone"
@@ -28,6 +46,10 @@ withTimezoneOffset zoneOffset =
     |> Setup.configure
 
 
+{-| A step that simulates waiting for some number of milliseconds to pass.
+
+Any subscriptions that depend on the passage of time will be triggered as expected.
+-}
 tick : Int -> Step.Context model -> Step.Command msg
 tick duration _ =
   Message.for "_time" "tick"
@@ -35,6 +57,10 @@ tick duration _ =
     |> Command.sendMessage
 
 
+{-| A step that simulates waiting for the next animation frame.
+
+Any subscriptions that depend on animation frame updates will be triggered as expected.
+-}
 nextAnimationFrame : Step.Context model -> Step.Command msg
 nextAnimationFrame _ =
   Command.sendMessage
