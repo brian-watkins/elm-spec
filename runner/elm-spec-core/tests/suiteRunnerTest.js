e976f1c103de5a200e0ad6dd5fdb89c29d12303e
Brian Watkins <brian.watkins@gmail.com>
2020-01-03 21:01:18 -0500

    Remove timeout from suite runner options in tests

--- runner/elm-spec-core/tests/suiteRunnerTest.js
+++ runner/elm-spec-core/tests/suiteRunnerTest.js
@@ -16,7 +16,7 @@ describe("Suite Runner", () => {
 
   context("when the suite should end on the first failure", () => {
     it("stops at the first failure", (done) => {
-      expectScenarios('WithFailure', { tags: [], timeout: 50, endOnFailure: true }, done, (observations) => {
+      expectScenarios('WithFailure', { tags: [], endOnFailure: true }, done, (observations) => {
         expect(observations).to.have.length(2)
         expectAccepted(observations[0])
         expectRejected(observations[1])
@@ -26,7 +26,7 @@ describe("Suite Runner", () => {
 
   context("when the suite should report all results", () => {
     it("reports all results", (done) => {
-      expectScenarios('WithFailure', { tags: [], timeout: 50, endOnFailure: false }, done, (observations) => {
+      expectScenarios('WithFailure', { tags: [], endOnFailure: false }, done, (observations) => {
         expect(observations).to.have.length(5)
         expectAccepted(observations[0])
         expectRejected(observations[1])
@@ -39,7 +39,7 @@ describe("Suite Runner", () => {
   
   context("when the code does not compile", () => {
     it("reports zero tests", (done) => {
-      expectScenarios('WithCompilationError', { tags: [], timeout: 50, endOnFailure: false }, done, (observations) => {
+      expectScenarios('WithCompilationError', { tags: [], endOnFailure: false }, done, (observations) => {
         expect(observations).to.have.length(0)
       })
     })
@@ -48,7 +48,7 @@ describe("Suite Runner", () => {
   context("when the expected ports are not found", () => {
     context("when the elmSpecOut port is not found", () => {
       it("fails and reports an error", (done) => {
-        expectScenarios("WithNoSendOutPort", { tags: [], timeout: 50, endOnFailure: false }, done, (observations, error) => {
+        expectScenarios("WithNoSendOutPort", { tags: [], endOnFailure: false }, done, (observations, error) => {
           expect(observations).to.have.length(0)
           expect(error).to.deep.equal([
             reportLine("No elmSpecOut port found!"),
@@ -60,7 +60,7 @@ describe("Suite Runner", () => {
 
     context("when the elmSpecIn port is not found", () => {
       it("fails and reports an error", (done) => {
-        expectScenarios("WithNoSendInPort", { tags: [], timeout: 50, endOnFailure: false }, done, (observations, error) => {
+        expectScenarios("WithNoSendInPort", { tags: [], endOnFailure: false }, done, (observations, error) => {
           expect(observations).to.have.length(0)
           expect(error).to.deep.equal([
             reportLine("No elmSpecIn port found!"),
@@ -73,7 +73,7 @@ describe("Suite Runner", () => {
 
   context("when the flags passed on init are messed up", () => {
     it("fails and reports an error", (done) => {
-      expectScenariosForVersion("BAD VERSION", "Passing", { tags: [], timeout: 50, endOnFailure: false }, done, (reporter) => {
+      expectScenariosForVersion("BAD VERSION", "Passing", { tags: [], endOnFailure: false }, done, (reporter) => {
         expect(reporter.observations).to.have.length(0)
         expect(reporter.errorCount).to.equal(1)
         expect(reporter.specError).to.not.be.null
@@ -83,7 +83,7 @@ describe("Suite Runner", () => {
 
   context("when the version is not correct", () => {
     it("fails and reports only one error", (done) => {
-      expectScenariosForVersion(-1, "Passing", { tags: [], timeout: 50, endOnFailure: false }, done, (reporter) => {
+      expectScenariosForVersion(-1, "Passing", { tags: [], endOnFailure: false }, done, (reporter) => {
         expect(reporter.observations).to.have.length(0)
         expect(reporter.errorCount).to.equal(1)
         expect(reporter.specError).to.not.be.null
@@ -94,7 +94,7 @@ describe("Suite Runner", () => {
 })
 
 const expectPassingScenarios = (specDir, number, tags, done) => {
-  expectScenarios(specDir, { tags: tags, timeout: 50, endOnFailure: false }, done, (observations) => {
+  expectScenarios(specDir, { tags: tags, endOnFailure: false }, done, (observations) => {
     expect(observations).to.have.length(number)
     observations.forEach(observation => {
       expect(observation.summary).to.equal("ACCEPT")
