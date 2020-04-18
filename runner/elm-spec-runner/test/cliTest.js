1fd1f327422829e0f9815a352b98b8b22c7ec270
Brian Watkins <brian.watkins@gmail.com>
2020-04-18 11:50:54 -0400

    Use ShellJs for better runner integration tests

--- runner/elm-spec-runner/test/cliTest.js
+++ runner/elm-spec-runner/test/cliTest.js
@@ -1,36 +1,56 @@
-const {expect, test} = require('@oclif/test')
-const cmd = require('..')
+const shell = require('shelljs')
+const {expect} = require('chai')
 
 describe('elm-spec-runner', () => {
+  context("when the spec runs", () => {
+    it("executes the spec and prints the number accepted", () => {
+      const command = "./bin/run" +
+        " --elm ../../node_modules/.bin/elm" +
+        " --cwd ../elm-spec-core/tests/sample/" +
+        " --specs ./specs/Passing/**/*Spec.elm"
+      const runnerOutput = shell.exec(command, { silent: true })
+      expect(runnerOutput.stdout).to.contain("Accepted: 8")
+    })
+  })
+
+  context("when files are watched", () => {
+    it("executes the spec once and waits for changes", async () => {
+      const command = "./bin/run" +
+        " --elm ../../node_modules/.bin/elm" +
+        " --cwd ../elm-spec-core/tests/sample/" +
+        " --specs ./specs/Passing/**/*Spec.elm" +
+        " --watch"
+      
+      const runnerOutput = await new Promise((resolve, reject) => {
+        const runner = shell.exec(command, { async: true, silent: true }, (code, stdout, stderr) => {
+          resolve(stdout)
+        })
+  
+        setTimeout(() => { runner.kill() }, 2000)  
+      })
+
+      expect(runnerOutput).to.contain("Watching Files")
+      expect(runnerOutput).to.contain("../elm-spec-core/tests/sample/src/**/*.elm")
+      expect(runnerOutput).to.contain("../elm-spec-core/tests/sample/specs/**/*.elm")
+      expect(runnerOutput).to.contain("Accepted: 8")
+    })
+  })
+
   context("when the specified elm executable does not exist", () => {
-    test
-    .do(async () => await cmd.run([
-      "--elm", "blah"
-    ]))
-    .catch(err => expect(err.message).to.contain("No elm executable found at: blah"))
-    .it('gives an error message')
+    it("prints an error", () => {
+      const command = "./bin/run --elm blah"
+      const runnerOutput = shell.exec(command, { silent: true })
+      expect(runnerOutput.stderr).to.contain("No elm executable found at: blah")
+    })
   })
 
-  context("when the elm.json cannot be found", () => {
-    test
-    .do(async () => await cmd.run([
-      "--elm", "../../node_modules/.bin/elm",
-      "--cwd", "../elm-spec-core/tests"
-    ]))
-    .catch(err => expect(err.message).to.contain("Expected an elm.json at: ../elm-spec-core/tests/elm.json"))
-    .it('prints an error message')
-  })
-
-  context("when the spec passes", () => {
-    test
-    .stdout()
-    .do(async () => await cmd.run([
-      "--elm", "../../node_modules/.bin/elm",
-      "--cwd", "../elm-spec-core/tests/sample/",
-      "--specs", "./specs/Passing/**/*Spec.elm",
-    ]))
-    .it('runs all the scenarios', (output) => {
-      expect(output.stdout).to.contain("Accepted: 8")
+  context("when the specs elm.json cannot be found", () => {
+    it("prints an error", () => {
+      const command = "./bin/run" +
+        " --elm ../../node_modules/.bin/elm" +
+        " --cwd ../elm-spec-core/tests/"
+      const runnerOutput = shell.exec(command, { silent: true })
+      expect(runnerOutput.stderr).to.contain("Expected an elm.json at: ../elm-spec-core/tests/elm.json")
     })
   })
 
